cf233e2a0fe552aa8aeb748306b9e14dabe10317
Uuuuser0810 <meditation_ol@163.com>
2018-05-03 16:05:44 +0800

    yonghujuese

---
+++ admin/src/main/java/com/jiyx/entity/Role.java
@@ -0,0 +1,47 @@
+package com.jiyx.entity;
+
+public class Role {
+    private int id;
+    private String role;
+    private String description;
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Role(int id, String role, String description) {
+
+        this.id = id;
+        this.role = role;
+        this.description = description;
+    }
+}
--- admin/src/main/java/com/jiyx/entity/User.java
+++ admin/src/main/java/com/jiyx/entity/User.java
@@ -1,4 +1,83 @@
 package com.jiyx.entity;
 
+import java.util.Date;
+
 public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private int mobile;
+    private int locked;
+    private Date create_time;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+
+    public int getLocked() {
+        return locked;
+    }
+
+    public void setLocked(int locked) {
+        this.locked = locked;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    public String getPassword() {
+
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(Integer id, String username, String password, String email, int mobile, int locked, Date create_time) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.mobile = mobile;
+        this.locked = locked;
+        this.create_time = create_time;
+    }
 }
